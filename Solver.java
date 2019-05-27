5908270d7b5181a3a5b6d5789f311dc618bcd69d
Ryan Lioy <rlioy1211@gmail.com>
2019-05-26 21:55:24 -0400

    Rewrote aStar() and added appropriate null checks

--- Solver.java
+++ Solver.java
@@ -1,4 +1,4 @@
-/*
+package MazeSolver;/*
 Implementation of the A* pathfinding algorithm
  */
 
@@ -12,17 +12,72 @@ public class Solver {
         this.graph = graph;
     }
 
-    public void aStar(Node start, Node finish){ // solves maze using A*
-    	ArrayList<Node> checked = new ArrayList<>();
-    	ArrayList<Node> open = new ArrayList<>(start);
-    	ArrayList<Node> backtrack = new ArrayList<>();
-    	gScore = 
+    public ArrayList<Node> aStar(Node start, Node finish) {
+        if (start == null || finish == null) { // no start/end node
+            return null;
+        }
+        ArrayList<Node> closeSet = new ArrayList<>(); // the Nodes that are not useful
+        ArrayList<Node> openSet = new ArrayList<>(); // the Nodes that might be useful
+        openSet.add(start);
+        ArrayList<Node> path = new ArrayList<>();
+        while (openSet.size() > 0) {
+            int bestF = 0;
+            for (int i = 0; i < openSet.size(); i++) { // find next least costly node
+                if (openSet.get(i).getF() < openSet.get(bestF).getF()) {
+                    bestF = i;
+                }
+            }
+            Node current = openSet.get(bestF);
 
+            if (current.equals(finish)) { // check if current node is end node
+                Node temp = current;
+                path.add(temp);
+                while (temp.getPrevious() != null) { // backtracking path
+                    path.add(temp.getPrevious());
+                    temp = temp.getPrevious();
+                }
+                return path;
+            }
+
+            openSet.remove(current);
+            closeSet.add(current);
+
+            ArrayList<Node> neighbors = current.getNeighbors();
+            for (int i = 0; i < neighbors.size(); i++) { // check neighbors
+                Node n = neighbors.get(i);
+
+                try {
+                    if (!closeSet.contains(n) && !n.isWall()) {
+                        double tempG = current.getG() + heuristic(current, n);
+                        if (!openSet.contains(n)) {
+                            openSet.add(n);
+                        }
+                        else if (tempG >= n.getG()) {
+                            continue;
+                        }
+                        n.setPrevious(current);
+                        n.setG(tempG);
+                        n.setF(n.getG() + heuristic(n, finish));
+                    }
+                }
+                catch (NullPointerException e) { // neighbors that do not exist are set to null and requires this
+                    continue;
+                }
+
+            }
+        }
+        return null; // no solution
     }
 
-    private void heuristic(Node end) {
-        double a = end.getCol();
-        double b = end.getRow();
-        end.setH(Math.sqrt(a * a + b * b)); // smallest distance to end
+    /*
+    This is the H in A* and the difference between A* and Dijkstra.
+     */
+    private static double heuristic(Node end, Node finish) {
+        int y1 = end.getCol();
+        int x1 = end.getRow();
+        int y2 = finish.getCol();
+        int x2 = finish.getRow();
+        return Math.abs(x2 - x1) + Math.abs(y2 - y1); // taxicab distance
+        //return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)); // Euclidean distance
     }
 }
