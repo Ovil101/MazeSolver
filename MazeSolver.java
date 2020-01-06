3bc4123b4380459210d9a016502b8855187ac0e7
Ovil101 <rlioy1211@gmail.com>
2020-01-05 20:59:12 -0500

    added no solution detection and impossilbe maze

--- MazeSolver.java
+++ MazeSolver.java
@@ -14,12 +14,19 @@ public class MazeSolver {
 
         long start = System.nanoTime();
         System.out.println("Solving using A* pathfinding");
-        System.out.println("Start time: "+start+"ns");
+        
 
         ArrayList<Node> solution = solve.aStar(a.findStartNode(nodes), a.findEndNode(nodes)); // olve maze
-        long finish = System.nanoTime();
-        System.out.println("End time: "+finish+"ns");
-        System.out.println("Time: "+(finish-start)+"ns ("+(finish-start)/1e9+"s)");
-        a.toImage(nodes, solution); // write image with solution
+        if (solution == null){
+            System.out.println("No solution found");
+        }
+        else{
+            long finish = System.nanoTime();
+            System.out.println("Start time: "+start+"ns");
+            System.out.println("End time: "+finish+"ns");
+            System.out.println("Time: "+(finish-start)+"ns ("+(finish-start)/1e9+"s)");
+            a.toImage(nodes, solution); // write image with solution
+        }
+        
     }
 }
